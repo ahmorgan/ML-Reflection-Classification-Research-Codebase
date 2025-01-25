01f4665625977d5f2fe7555124adfc1a14c29946
Andrew Morgan <163063003+ahmorgan@users.noreply.github.com>
2025-01-25 00:36:42 -0500

    support added for single-label classification in compute_metrics

--- SetFit Implementation/model.py
+++ SetFit Implementation/model.py
@@ -1,10 +1,11 @@
 from datasets import load_dataset
 from setfit import SetFitModel, Trainer, TrainingArguments
-from sklearn.metrics import multilabel_confusion_matrix
+from sklearn.metrics import multilabel_confusion_matrix, confusion_matrix, ConfusionMatrixDisplay, f1_score, classification_report
 from optuna import Trial
 import numpy
 import csv
 import torch
+from matplotlib import pyplot as plt
 
 
 # Generate a confusion matrix for each label in the dataset. For each column/vector
@@ -14,51 +15,82 @@ import torch
 # for each class, a minimally noisy confusion matrix can be created for each label
 def compute_metrics(y_pred, y_true) -> dict[str, float]:
     # initialize labels
-    labels = ["Python and Coding",
-              "Github",
-              "Assignments",
-              "Time Management and Motivation",
-              ]
+    labels = ["API", 'Course Structure and Materials', 'Github', 'Group Work', 'MySQL', 'No Issue',
+              'Python and Coding', 'Time Management and Motivation']
+    if not any(item in y_true for item in [i for i in range(2, max(y_true))]):  # MULTI-LABEL CASE if y_true doesn't contain numbers other than 0,1
+        # save the raw predictions made by the model
+        with open("raw_setfit_preds.csv", "w", encoding="utf-8", newline='') as rsp:
+            c_w = csv.writer(rsp)
+            for i in range(0, len(y_true)):
+                row = []
+                for j in range(0, len(labels)):
+                    row.append(y_pred[i][j].item())
+                c_w.writerow(row)
 
-    # save the raw predictions made by the model
-    with open("raw_setfit_preds.csv", "w", encoding="utf-8", newline='') as rsp:
-        c_w = csv.writer(rsp)
-        for i in range(0, len(y_true)):
-            row = []
-            for j in range(0, len(labels)):
-                row.append(y_pred[i][j].item())
-            c_w.writerow(row)
-            
-    # confusion_matrices is a list of n-dimensional numpy arrays
-    # list is of size num_labels
-    confusion_matrices = multilabel_confusion_matrix(y_true, y_pred)
-    
-    result = {}
-    x = 0
-    for matrix in confusion_matrices:
-        # flatten confusion matrix to list
-        matrix = matrix.ravel()
-        # populate results with information from the label's confusion matrix
-        result.update({f"{labels[x]}-tn": matrix[0].item()})
-        result.update({f"{labels[x]}-fp": matrix[1].item()})
-        result.update({f"{labels[x]}-fn": matrix[2].item()})
-        result.update({f"{labels[x]}-tp": matrix[3].item()})
-        x += 1
-        if x >= len(labels):
-            break
-    accuracy = 0.0
-    for label in labels:
-        # len(y_true) is the number of reflections used in evaluation
-        # acc = num_of_correct_classifications / num_reflections
-        accuracy += (result[f"{label}-tp"] + result[f"{label}-tn"]) / len(y_true)
-    accuracy /= len(labels)
-    result.update({"accuracy": accuracy})
-    return result
+        # confusion_matrices is a list of n-dimensional numpy arrays
+        # list is of size num_labels
+        confusion_matrices = multilabel_confusion_matrix(y_true, y_pred)
+
+        result = {}
+        x = 0
+        for matrix in confusion_matrices:
+            # flatten confusion matrix to list
+            matrix = matrix.ravel()
+            # populate results with information from the label's confusion matrix
+            result.update({f"{labels[x]}-tn": matrix[0].item()})
+            result.update({f"{labels[x]}-fp": matrix[1].item()})
+            result.update({f"{labels[x]}-fn": matrix[2].item()})
+            result.update({f"{labels[x]}-tp": matrix[3].item()})
+            x += 1
+            if x >= len(labels):
+                break
+        accuracy = 0.0
+        print(len(y_true))
+        for label in labels:
+            # len(y_true) is the number of reflections used in evaluation
+            # acc = num_of_correct_classifications / num_reflections
+            accuracy += (result[f"{label}-tp"] + result[f"{label}-tn"]) / len(y_true)
+        accuracy /= len(labels)
+        result.update({"accuracy": accuracy})
+        return result
+    else:
+        matrix = confusion_matrix(y_true, y_pred, labels=[i for i in range(0, max(y_true) + 1)])
+        report = classification_report(y_true, y_pred, labels=[i for i in range(0, max(y_pred) + 1)],
+                                       target_names=labels, output_dict=True)
+        f1 = f1_score(y_true, y_pred, average="macro")
+
+        with open("results.csv", "w", encoding="utf-8", newline="") as results:
+            c_w = csv.writer(results)
+            c_w.writerow(labels)
+            for row in matrix:
+                c_w.writerow(row)
+            c_w.writerow([])
+            for label in report.keys():
+                c_w.writerow([label])
+                if type(report[label]) != dict:
+                    if type(report[label]) == float:
+                        report[label] = [report[label]]
+                    c_w.writerow(report[label])
+                else:
+                    for item in report[label].items():
+                        c_w.writerow(item)
+                c_w.writerow([])
+
+        with open("raw_results.csv", "w", encoding="utf-8", newline="") as rr:
+            c_w = csv.writer(rr)
+            for pred in y_pred:
+                c_w.writerow([labels[pred]])
+
+        display = ConfusionMatrixDisplay(confusion_matrix=matrix, display_labels=labels)
+        display.plot()
+        plt.show()
+        return {"F1": f1}
 
 
 # model instantiation for each trial run of the hyperparameter search
 def model_init(params):
-    params = {"multi_target_strategy": "one-vs-rest", "device": torch.device("cuda")}
+    params = {  # "multi_target_strategy": "one-vs-rest",
+              "device": torch.device("cuda")}
     # all-MiniLM-L12-v2 is 33.6M params
     return SetFitModel.from_pretrained("sentence-transformers/all-MiniLM-L12-v2", **params)
 
@@ -66,7 +98,7 @@ def model_init(params):
 # hyperparameters to optimize during hp search
 def hp_space(trial: Trial):
     return {
-        "body_learning_rate": trial.suggest_float("body_learning_rate", 1e-6, 1e-3, log=True),
+        "body_learning_rate": trial.suggest_float("body_learning_rate", 1e-5, 1e-3, log=True),
         "num_epochs": trial.suggest_int("num_epochs", 1, 3)
     }
 
@@ -89,36 +121,40 @@ def main():
     })
 
     print("Processing datasets...")
-    # extract labels
+    # extract the header column in the dataset
     labels = dataset["train"].column_names
-    labels.remove("text")
+    if len(labels) > 2:  # len(labels) > 2 indicates a multi-label dataset
+        print("Multi-label dataset detected, doing preprocessing...")
+        labels.remove("text")
 
-    # further preprocess data
-    # used guide https://medium.com/@farnazgh73/few-shot-text-classification-on-a-multilabel-dataset-with-setfit-e89504f5fb75 for help here
-    # .map takes a method and applies it to each entry in the dataset
-    # the lambda method converts the entries in the dataset to encoded labels
-    # ex. {"Time Management":0, "Python and Coding": 1} becomes {"label": [0,1]} (not a real example, just to illustrate what's happening)
-    dataset["train"] = dataset["train"].map(lambda entry: {"label": [entry[label] for label in labels]})
-    dataset["test"] = dataset["test"].map(lambda entry: {"label": [entry[label] for label in labels]})
+        # further preprocess data
+        # used guide https://medium.com/@farnazgh73/few-shot-text-classification-on-a-multilabel-dataset-with-setfit-e89504f5fb75 for help here
+        # .map takes a method and applies it to each entry in the dataset
+        # the lambda method converts the entries in the dataset to encoded labels
+        # ex. {"Time Management":0, "Python and Coding": 1} becomes {"label": [0,1]} (not a real example, just to illustrate what's happening)
+        dataset["train"] = dataset["train"].map(lambda entry: {"label": [entry[label] for label in labels]})
+        dataset["test"] = dataset["test"].map(lambda entry: {"label": [entry[label] for label in labels]})
 
-    # collect exactly eight examples of every labeled class in training dataset
-    # elegant line of code taken from above medium.com guide
-    eight_examples_of_each = numpy.concatenate([numpy.random.choice(numpy.where(dataset["train"][label])[0], 10) for label in labels])
-    # replace training dataset with the eight examples of each
-    dataset["train"] = dataset["train"].select(eight_examples_of_each)
+        # collect exactly eight examples of every labeled class in training dataset
+        # elegant line of code taken from above medium.com guide
+        eight_examples_of_each = numpy.concatenate([numpy.random.choice(numpy.where(dataset["train"][label])[0], 10) for label in labels])
+        # replace training dataset with the eight examples of each
+        dataset["train"] = dataset["train"].select(eight_examples_of_each)
 
-    # remove unnecessary labels
-    dataset["train"] = dataset["train"].select_columns(["text", "label"])
-    dataset["test"] = dataset["test"].select_columns(["text", "label"])
+        # remove unnecessary labels
+        dataset["train"] = dataset["train"].select_columns(["text", "label"])
+        dataset["test"] = dataset["test"].select_columns(["text", "label"])
 
-    # dataset["train"] is now a collection of 8*num_labels reflections, where there are at least 8
-    # reflections with a certain label (there could be more because the dataset is multi-label)
-    # dataset["train"] has not had any reflections removed. All that has happened to it is that the
-    # labels for each reflection have been encoded into an entry with the form {"label":[0,0,1,...0])
+        # dataset["train"] is now a collection of 8*num_labels reflections, where there are at least 8
+        # reflections with a certain label (there could be more because the dataset is multi-label)
+        # dataset["train"] has not had any reflections removed. All that has happened to it is that the
+        # labels for each reflection have been encoded into an entry with the form {"label":[0,0,1,...0])
 
-    # therefore, the model will train on eight examples of each label, and metrics will be computed based on
-    # on classifications made from a large set of reflections in a randomized order
-    # no reflection from the test split will be in the train split, so over-fitting should not be a concern
+        # therefore, the model will train on eight examples of each label, and metrics will be computed based on
+        # on classifications made from a large set of reflections in a randomized order
+        # no reflection from the test split will be in the train split, so over-fitting should not be a concern
+
+    # In the single label case, the data is already prepared for classification
 
     # tokenization as specified in the "Fine tuning BERT (and friends)" notebook is not necessary or worthwhile
     # (as far as I know) working with SetFit models. SetFit must tokenize the data behind the scene
@@ -127,9 +163,9 @@ def main():
 
     # only setting initial batch size, hyperparameter search will cover learning rate and num epochs
     args = TrainingArguments(
-        batch_size=16,
-        body_learning_rate=0.00017039160219643772,  # optimal lr determined through hp search
-        num_epochs=1
+        batch_size=8,
+        body_learning_rate=0.0001037,  # optimal lr determined through hp search
+        num_epochs=2
     )
 
     # fine tune pretrained model using datasets using default hyperparameters (will change as I run experiments with
@@ -143,17 +179,17 @@ def main():
     )
 
     print("Training...")
-    # optimizing sentence transformer learning rate and num of epochs with hyperparameter search
     """
+    # optimizing sentence transformer learning rate and num of epochs with hyperparameter search
     best_run = trainer.hyperparameter_search(
         # compute_objective is the overall accuracy of all labels
         direction="maximize",  # maximize accuracy
         hp_space=hp_space,
-        compute_objective=lambda result: result.get("accuracy"),
+        compute_objective=lambda result: result.get("F1"),
         n_trials=20
     )
-    trainer.apply_hyperparameters(best_run.hyperparameters)
     """
+    # trainer.apply_hyperparameters(best_run.hyperparameters)
 
     trainer.train()
 
@@ -171,6 +207,8 @@ def main():
             arr = [key, metrics[key]]
             c_w.writerow(arr)
     print("Metrics data written to metrics.csv")
+
+    print(torch.cuda.memory_summary())
 
 
 if __name__ == "__main__":
