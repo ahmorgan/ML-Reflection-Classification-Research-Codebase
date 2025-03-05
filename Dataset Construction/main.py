5fd2842385b3c49e0b08ea415daf094467081f1a
Andrew Morgan <163063003+ahmorgan@users.noreply.github.com>
2025-03-05 01:41:42 -0500

    Made sub-reflection processing more robust, some superficial refactoring

--- Dataset Construction/main.py
+++ Dataset Construction/main.py
@@ -1,15 +1,6 @@
 # DATASET PREPROCESSOR: goes from unprocessed annotation data to multi-label dataset
 # for training a model for multi-label classification
 
-# ********BEFORE RUNNING MAIN DO THIS*************:
-# pre-pre-pre-processing to do first: get rid of "ID" column in each excel in raw_data
-# and remove annotation sets that are not complete
-# also make sure that column g is the issue column in every dataset.
-# also make sure that none of the "issue" labels are null (either fix or remove them if so)
-# also make sure none of the cells in any of the header rows are null.
-# i could probably do these programmatically but the datasets are so messy it's
-# just less of a pain to do these first steps manually
-
 import csv
 import pandas as pd
 import numpy as np
@@ -68,7 +59,7 @@ primary_labels = []
 # output_file: name to assign to file which contains the final consensus dataset
 # include_other: whether or not to include the "other" column. If not, any reflection with the "other"
 #   label will remain in the dataset, just stripped of the "other" label
-def process(files, output_file, dataset_name):
+def construct_dataset(files, output_file, dataset_name):
     print(f"Processing files in {dataset_name}...\n")
     # CONSENSUS LABELING METHODOLOGY
     # **************************************
@@ -113,6 +104,8 @@ def process(files, output_file, dataset_name):
             reflection_labels = []  # n labels chosen by annotator for that reflection
             i = 0
             for row in c_r:
+                if current_reflection == "😍Excited <name omitted> is the best <name omitted> is the best <name omitted> is the best <name omitted> is the best":
+                    print(f"found in {file}")
                 # row[0] == reflection text, row[1] == label
                 if current_reflection != row[0]:
                     # create a new entry in the dictionary if one doesn't exist for the current reflection
@@ -208,39 +201,55 @@ def process(files, output_file, dataset_name):
 # method returns None
 # METHOD PARAMETERS:
 # reflections: reflections_new from process()
-def sanitize_gpt_reflections(reflections):
+def align_subreflections(reflections):
+    print(len(reflections))
     # full_refs is a list of every reflection with no label exclusions
     # which will be populated by gpt_reflections.csv
     full_refs = []
     # copy full reflection data w/o sub-responses
     # with no label exclusion from gpt_reflections.csv into full_refs
     with open("gpt_reflections.csv", "r", encoding="utf-8") as gpt:
-        c_r = csv.reader(gpt)
+        c_r = list(csv.reader(gpt))
+        print(len(c_r))
         for row in c_r:
-            ref = ""
+            ref = " ".join(row)
             # concatenate each part in full_refs
+            """
             for i in range(0, len(row)):
                 if i == len(row)-1:
                     ref += row[i]
                 else:
                     ref += row[i] + ' '
-            full_refs.append(ref)
-    del full_refs[0]
+            """
+            print(f"Reflection: {ref}")
+            if len(full_refs) > 0:
+                print(f"Last in full_refs: {' '.join(full_refs[-1])}")
+            else:
+                print("First")
+            if len(full_refs) == 0 or " ".join(full_refs[-1]) != ref:
+                print(f"Appended ref: {ref}")
+                full_refs.append(row)
+            else:
+                print("Ref skipped!")
+            print()
+    print(len(full_refs))
     # indices of rows to remove from dataframe of gpt_reflections
     # each row referenced in remove is one in full_refs
     remove = []
     # locate rows which are in full_refs but not in the label-truncated
     # reflections parameter, read their indices into remove
-    for ref in full_refs:
-        if ref not in reflections:
-            remove.append(full_refs.index(ref))
+    for i in range(0, len(full_refs)):
+        if " ".join(full_refs[i]) not in reflections:
+            remove.append(i)
     # dataframe of original gpt_reflections, each row contains the 5 sub-responses making
     # up the entire reflections
-    df = pd.read_csv("gpt_reflections.csv")
+    df = pd.DataFrame(full_refs)
     # drop undesired rows simultaneously and overwrite gpt_reflections.csv with new truncated data
+    print(len(df))
     if remove:
         df.drop(remove, inplace=True)
-    df.to_csv("gpt_reflections.csv", index=False, header=False)
+    print(len(df))
+    df.to_csv("reflection_substrings.csv", index=False, header=False)
 
     # Note: I can't just return a csv of reflections_sanitized because I want gpt_reflections.csv
     # to be divided into sub-parts (that is, each of the sub-responses to each question in the original
@@ -248,7 +257,8 @@ def sanitize_gpt_reflections(reflections):
     # questions to each sub-response included
 
 
-# Test method to make sure that full_dataset.csv and label_sets.csv contain the same reflections and labels
+# Test method to make sure that full_dataset.csv and label_sets.csv contain the same reflections and labels,
+# and also to verify that the consensus labels are correct
 def validate_datasets():
     full = []
     label_sets = []
@@ -287,8 +297,24 @@ def validate_datasets():
         for label_enc in consensus_labels_encoded:
             assert full[i+1][label_enc] == "1", f"Consensus label mismatch at reflection number {i}, reflection {full[i+1][-1]}!"
 
+    with open("reflection_substrings.csv", "r", encoding="utf-8") as gr:
+        c_r = list(csv.reader(gr))
+        gpt_refs = [' '.join(row) for row in c_r]
+        refs_actual = [row[0] for row in label_sets]
+        assert len(gpt_refs) == len(refs_actual), f"Substring refs and actual refs have differing lengths, {len(gpt_refs)}, {len(refs_actual)}"
+        for actual, gpt in zip(gpt_refs, refs_actual):
+            assert gpt.rstrip() == actual.rstrip()
+
 
 def main():
+    # Manual pre-processing for the datasets: get rid of "ID" column in each excel in raw_data
+    # and remove annotation sets that are not complete
+    # also make sure that column g is the issue column in every dataset.
+    # also make sure that none of the "issue" labels are null (either fix or remove them if so)
+    # also make sure none of the cells in any of the header rows are null.
+    # i could probably do these programmatically but the datasets are so messy it's
+    # just less of a pain to do these first steps manually
+
     # Instructions: identify what labels should be kept in the final dataset by altering "exclude_labels"
     # and/or "include_other" (which includes the "Other" label class)
     # Ensure that this folder -> https://drive.google.com/drive/folders/10g5msqE4sELGakqICucO9sVuxlXoIggM?usp=drive_link
@@ -297,16 +323,16 @@ def main():
     # Refer to the top of this file for instructions on minor manual dataset cleaning to be done first
     # This code will output a multi-label dataset for each sub-dataset (each D-ESX-X dataset) as well as
     # "gpt_reflections.csv", which is used in my GPT-4o implementation as part of the prompt
-    
-    # Superset of labels as of 1/28: 
+
+    # Superset of labels as of 1/28:
     # [None, Python and Coding, GitHub, MySQL, Assignments, Quizzes, Learning New Material, Understanding requirements and instructions,
     # Course Structure and Materials, Time Management, Group Work, IDE and Package Installation, Personal Issue, API, HTML, SDLC, Other Primary]
     # All labels in the data but not in the superset are excluded
     #
     # Choose which labels to exclude in the final generated multilabel training dataset
-    exclude_labels = ["Assignments", "Quizzes", "Learning New Material", "Understanding requirements and instructions", "Personal Issue"]
+    exclude_labels = ["Assignments", "Quizzes", "Learning New Material", "Personal Issue"]
     # TODO write in support for secondary labels
-    label_category = "Primary"  # CAUTION: as of 1/14 I have not written in full support for the secondary label category -- COMING SOON
+    label_category = "Primary"  # CAUTION: as of 1/28 I have not written in full support for the secondary label category -- COMING SOON
 
     if exclude_labels:
         # remove unwanted labels
@@ -345,7 +371,7 @@ def main():
     if not os.path.isdir("data"):
         os.makedirs("data")
     if len(os.listdir("data")) == 0:
-        organize.organize(label_category=label_category)  # wrangle the raw datasets into a format that can be further processed
+        organize.prepare_raw_data(label_category=label_category)  # wrangle the raw datasets into a format that can be further processed
         # into a multi-label training dataset
 
     full_dataset = []
@@ -359,7 +385,7 @@ def main():
 
         # process() generates multi-label training dataset for each ESU/P dataset
         # and then returns list of reflections for that dataset
-        package = process(files=files, output_file=output_file, dataset_name=sub_dir)
+        package = construct_dataset(files=files, output_file=output_file, dataset_name=sub_dir)
 
         # package is the [0] the list of reflections to undergo further preprocessing for GPT code
         # and [1] the label sets and their corresponding reflections for calculating Krippendorff's alpha
@@ -390,7 +416,7 @@ def main():
 
     # gpt_reflections is every reflection in raw_data divided in it's requisite sub-parts by default
     # based on reflections_sanitized which has been molded in accordance with the label exclusions,
-    sanitize_gpt_reflections(reflections_sanitized)
+    align_subreflections(reflections_sanitized)
 
     # test method that ensures that full_dataset.csv and label_sets.csv contain the same reflections
     # can throw AssertionError
